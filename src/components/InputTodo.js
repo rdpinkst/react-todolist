e16ebde51cbfdd9f80e3cfb41083ecc58d2a26c4
Ryan P <rdpinkstaff@gmail.com>
2022-05-15 17:28:30 -0600

    Got the form styled

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,7 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/inputTodo.css";
 
 function InputTodo({ show, setShow }) {
+  const [todo, setTodo] = useState(true);
+  const [project, setProject] = useState(false);
+
+  const styleTodo = {
+    textDecoration: todo ? "underline" : "",
+    fontWeight: todo ? "700" : "",
+    color: todo ? "#ef9273" : "",
+  };
+  const styleProject = {
+    textDecoration: project ? "underline" : "",
+    fontWeight: project ? "700" : "",
+    color: project ? "#ef9273" : "",
+  };
+
+  function clickProject() {
+    if (todo) {
+      setTodo((prevState) => !prevState);
+      setProject((prevState) => !prevState);
+    }
+  }
+
+  function clickTodo() {
+    if (project) {
+      setProject((prevState) => !prevState);
+      setTodo((prevState) => !prevState);
+    }
+  }
+
   useEffect(() => {
     function clickClose(e) {
       if (e.target.className === "close") {
@@ -16,34 +44,56 @@ function InputTodo({ show, setShow }) {
 
   return (
     <form className="input-card">
-      <p className="close">x</p>  
-      <div>
-        <label htmlFor="todo-name">Task to Complete</label>
+      <div className="input-selection">
+        <p className="selection-input" style={styleTodo} onClick={clickTodo}>
+          New Todo
+        </p>
+        <p
+          className="selection-input"
+          style={styleProject}
+          onClick={clickProject}
+        >
+          New Project
+        </p>
+      </div>
+      <p className="close">x</p>
+      {todo && <div>
+       <div className="input">
+        <label htmlFor="todo-name">Task to Complete:</label>
         <input type="text" id="todo-name" name="todoTitle" />
       </div>
-      <div>
-        <label htmlFor="todo-description">Task Description</label>
+      <div className="input">
+        <label htmlFor="todo-description">Task Description:</label>
         <textarea
           type="text"
           id="todo-description"
           name="description"
         ></textarea>
       </div>
-      <div>
-        <label htmlFor="todo-date">Date</label>
+      <div className="input">
+        <label htmlFor="todo-date">Date:</label>
         <input type="date" id="todo-date" name="date" />
       </div>
-      <div>
-        <label htmlFor="todo-urgency">Urgency</label>
+      <div className="input">
+        <label htmlFor="todo-urgency">Urgency:</label>
         <select>
           <option value="low">Low</option>
           <option value="medium">Medium</option>
           <option value="High">High</option>
         </select>
       </div>
-      <div>
+      <div className="input">
         <button>Submit Todo</button>
-      </div>
+      </div></div>}
+      {project && <div>
+        <div className="input">
+          <label htmlFor="project-input">Project Name</label>
+          <input type="text" id="project-input" name="project" />
+        </div>
+        <div className="input">
+          <button>Submit Project</button>
+        </div>
+        </div>}
     </form>
   );
 }
